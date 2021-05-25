f46c82b84d1a27a8c5f00d84f5ca4a2d4ec35868
Zinko17 <vovazin2@gmail.com>
2021-05-25 16:08:44 +0600

    create: homepage view, service page for list of services and master for list of masters

--- django_project/spa/views.py
+++ django_project/spa/views.py
@@ -1,5 +1,15 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Service
+from  .models import Master
 
 def homepage(request):
     return HttpResponse('Сайт')
+
+def services(request):
+    services = Service.objects.all()
+    return render(request,'service.html',{'services':services})
+
+def masters(request):
+    masters = Master.objects.all()
+    return render(request,'master.html',{'masters':masters})
