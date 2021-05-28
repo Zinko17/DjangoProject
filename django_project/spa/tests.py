353daecc70f45bae579632f4daeef5b594bc710c
Zinko17 <vovazin2@gmail.com>
2021-05-28 18:35:42 +0600

    create test for Service model

--- django_project/spa/tests.py
+++ django_project/spa/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from .models import Service,Master
+from django.urls import reverse
 
-# Create your tests here.
+
+class ServiceTest(TestCase):
+
+    def setUp(self):
+        self.url = reverse('services')
+        self.master = Master.objects.create(full_name='maksim',exp=2,birth_date="2021-09-09")
+        Service.objects.create(name='zagar',price=900,master=self.master)
+
+    def test_service_get(self):
+        self.response = self.client.get(self.url)
+        self.assertEqual(self.response.status_code,200)
+
+
