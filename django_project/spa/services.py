cdb3dcb754db5c8ce12979e10f261277beb1c559
Zinko17 <vovazin2@gmail.com>
2021-05-31 18:20:12 +0600

    add function dor check validity of the certificate

--- django_project/spa/services.py
+++ django_project/spa/services.py
@@ -1,7 +1,19 @@
 from user_profile.models import Profile
+import datetime
+
 
 def profileCreate(profile_data,instance):
     name = profile_data.get('name')
     age = profile_data.get('age')
     email = profile_data.get('email')
     Profile.objects.create(name=name,age=age,email=email,user=instance)
+
+
+def checkExpired(certificates):
+    for certificate in certificates:
+        print(certificates)
+        date_expired = certificate.date_expired
+        today = datetime.date.today()
+        if today > date_expired:
+            certificate.status = 'dead'
+            certificate.save()
