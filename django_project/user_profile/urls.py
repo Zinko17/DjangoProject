24e56e19368edf6a6a52553de23b35e05884528d
Zinko17 <vovazin2@gmail.com>
2021-05-31 18:20:51 +0600

    add path for my_order view and delete_order view

--- django_project/user_profile/urls.py
+++ django_project/user_profile/urls.py
@@ -4,4 +4,6 @@ from .views import *
 urlpatterns = [
     path('profile/', profile_page),
     path('order/<int:service_id>', order_page,name='order'),
+    path('order/',my_orders,name='my_orders'),
+    path('delete_order/<int:order_id>/',delete_order)
 ]
