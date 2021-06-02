73669bf497ed419d526862109e4bfbc12e5958ee
Zinko17 <vovazin2@gmail.com>
2021-06-02 18:30:44 +0600

    add path for update_order view

--- django_project/user_profile/urls.py
+++ django_project/user_profile/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('profile/', profile_page),
     path('order/<int:service_id>', order_page,name='order'),
     path('order/',my_orders,name='my_orders'),
-    path('delete_order/<int:order_id>/',delete_order)
+    path('delete_order/<int:order_id>/',delete_order),
+    path('update_order/<int:order_id>/',update_order),
 ]
