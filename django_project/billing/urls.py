ca6dd8f5e397c4eb641615c8419a716c3a350501
Zinko17 <vovazin2@gmail.com>
2021-06-02 18:30:06 +0600

    add name for card_create path

--- django_project/billing/urls.py
+++ django_project/billing/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('card_create/', cardCreate),
+    path('card_create/', cardCreate,name='card_crete'),
     path('increment/', incrementBalance),
     path('transaction/', transactionPage),
 
