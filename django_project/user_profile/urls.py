74b84df353662aef7de749d9d975eb8ccdd9dbb7
Zinko17 <vovazin2@gmail.com>
2021-06-03 18:31:28 +0600

    add names for path

--- django_project/user_profile/urls.py
+++ django_project/user_profile/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('profile/', profile_page),
-    path('order/<int:service_id>', order_page,name='order'),
+    path('profile/', profile_page,name='profile'),
+    path('order/<int:service_id>',order_page,name='order'),
     path('order/',my_orders,name='my_orders'),
-    path('delete_order/<int:order_id>/',delete_order),
-    path('update_order/<int:order_id>/',update_order),
+    path('delete_order/<int:order_id>/',delete_order,name='delete_order'),
+    path('update_order/<int:order_id>/',update_order,name='update_order'),
 ]
