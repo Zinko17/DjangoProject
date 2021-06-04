dd3cdc12fd1763c3b8039470fdb2d7dad95a7458
Zinko17 <vovazin2@gmail.com>
2021-06-04 17:30:02 +0600

    create form for update profile

--- django_project/user_profile/forms.py
+++ django_project/user_profile/forms.py
@@ -1,8 +1,15 @@
 from django.forms import ModelForm
-from .models import Order
+from .models import *
 
 
 class OrderForm(ModelForm):
     class Meta:
         model = Order
         fields = ['user', 'service','payment_method']
+
+
+class ProfileForm(ModelForm):
+
+    class Meta:
+        model = Profile
+        fields = ['name','photo','age','email']
