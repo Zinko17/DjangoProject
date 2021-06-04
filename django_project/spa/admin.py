2ca3e7226359485950a539e05e64e3658eef6c82
Zinko17 <vovazin2@gmail.com>
2021-06-04 17:29:23 +0600

    add view for service master and certificate as table

--- django_project/spa/admin.py
+++ django_project/spa/admin.py
@@ -2,7 +2,15 @@ from django.contrib import admin
 
 from .models import Service,Master,Certificate
 
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ['name','price','master']
+admin.site.register(Service,ServiceAdmin)
 
-admin.site.register(Service)
-admin.site.register(Master)
-admin.site.register(Certificate)
+
+class MasterAdmin(admin.ModelAdmin):
+    list_display = ['full_name','photo','exp','birth_date']
+admin.site.register(Master,MasterAdmin)
+
+class CertAdmin(admin.ModelAdmin):
+    list_display = ['name','date_graduate','date_expired','status']
+admin.site.register(Certificate,CertAdmin)
